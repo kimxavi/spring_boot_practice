da9f46c3c9b098e92597b660af4a1bdce0b2d799
kkw <kkw@192.168.30.2>
2016-08-11 22:50:40 +0900

    modify webcontroller

--- Promotion/src/main/java/com/promotion/project/controller/PromotionWebController.java
+++ Promotion/src/main/java/com/promotion/project/controller/PromotionWebController.java
@@ -4,14 +4,11 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/promotion")
 public class PromotionWebController {
 	@GetMapping("/crud")
 	public String welcome(Map<String, Object> model) {
-	
 		return "crud";
 	}
 
